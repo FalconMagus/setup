cf6b007a1e3a5ec15e001094e340e679545ca1d1
David Carroll <Dessyreqt@users.noreply.github.com>
2020-03-09 23:02:55 -0500

    Create _init.ps1

---
+++ _init.ps1
@@ -0,0 +1,7 @@
+# Copy paste this to an admin PowerShell
+
+. { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force
+
+# Once the above is done, run this command
+
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/Dessyreqt/setup/master/standard.txt -DisableReboots
